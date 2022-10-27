b3730aad6f8a7aba8e4ce9bc366d5ce3afd62f0a
Jeff Fletcher <fletch.jeff@gmail.com>
2022-10-27 09:36:52 +0200

    DAG Update

--- dags/astro_orders.py
+++ dags/astro_orders.py
@@ -14,27 +14,6 @@ POSTGRES_ORDERS = "orders_table"
 POSTGRES_CUSTOMERS = "customers_table"
 POSTGRES_REPORTING = "reporting_table"
 
-# Define an SQL query for our transform step as a Python function using the SDK.
-# This function filters out all rows with an amount value less than 150.
-@aql.transform
-def filter_orders(input_table: Table):
-    return "SELECT * FROM {{input_table}} WHERE amount > 150"
-
-# Define an SQL query for our transform step as a Python function using the SDK.
-# This function joins two tables into a new table.
-@aql.transform
-def join_orders_customers(filtered_orders_table: Table, customers_table: Table):
-    return """SELECT c.customer_id, customer_name, order_id, purchase_date, amount, type
-    FROM {{filtered_orders_table}} f JOIN {{customers_table}} c
-    ON f.customer_id = c.customer_id"""
-
-# Define a function for transforming tables to dataframes
-@aql.dataframe
-def transform_dataframe(df: DataFrame):
-    purchase_dates = df.loc[:, "purchase_date"]
-    print("purchase dates:", purchase_dates)
-    return purchase_dates
-
 # Basic DAG definition. Run the DAG starting January 1st, 2019 on a daily schedule.
 dag = DAG(
     dag_id="astro_orders",
@@ -44,11 +23,13 @@ dag = DAG(
 )
 
 with dag:
+
     # Extract a file with a header from the web into a temporary Table, referenced by the
     # variable `orders_data`
     orders_data = aql.load_file(
         # Data file needs to have a header row. The input and output table can be replaced with any
         # valid file and connection ID.
+        task_id="fetch_orders_data_file",
         input_file=File(
             path=HTTP_FILE_PATH + "/orders_data_header.csv",
         ),
@@ -60,6 +41,20 @@ with dag:
         name=POSTGRES_CUSTOMERS,
         conn_id=POSTGRES_CONN_ID,
     )
+
+    # Define an SQL query for our transform step as a Python function using the SDK.
+    # This function filters out all rows with an amount value less than 150.
+    @aql.transform
+    def filter_orders(input_table: Table):
+        return "SELECT * FROM {{input_table}} WHERE amount > 150"
+
+    # Define an SQL query for our transform step as a Python function using the SDK.
+    # This function joins two tables into a new table.
+    @aql.transform
+    def join_orders_customers(filtered_orders_table: Table, customers_table: Table):
+        return """SELECT c.customer_id, customer_name, order_id, purchase_date, amount, type
+        FROM {{filtered_orders_table}} f JOIN {{customers_table}} c
+        ON f.customer_id = c.customer_id"""        
 
     # Filter the orders data and then join with the customer table,
     # saving the output into a temporary table referenced by the Table instance `joined_data`
@@ -80,12 +75,21 @@ with dag:
     # )
 
     reporting_table = aql.append(
+        task_id="append_to_reporting_table",
         target_table=Table(
             name=POSTGRES_REPORTING,
             conn_id=POSTGRES_CONN_ID,
         ),
         source_table=joined_data,
     )
+
+    # Define a function for transforming tables to dataframes
+    @aql.dataframe
+    def transform_dataframe(df: DataFrame):
+        purchase_dates = df.loc[:, "purchase_date"]
+        print("purchase dates:", purchase_dates)
+        return purchase_dates
+
 
     # Transform the reporting table into a dataframe
     purchase_dates = transform_dataframe(reporting_table)
